79290de51dcd909f4c1ba0aa2dd6bfd04aec651a
Rostysikk <st9246435@stud.duikt.edu.ua>
2026-03-18 20:16:00 +0200

    Add pr2

---
+++ Pr2_2semestr/Pr2_2semestr/AirConditioner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class AirConditioner
+{
+    public void Subscribe(TemperatureSensor sensor)
+    {
+        sensor.TemperatureChanged += ReactToTemperature;
+    }
+
+    private void ReactToTemperature(double temperature)
+    {
+        if (temperature < 17)
+            Console.WriteLine("AirConditioner: HEATING is on");
+        else if (temperature <= 25)
+            Console.WriteLine("AirConditioner: Air conditioning OFF");
+        else
+            Console.WriteLine("AirConditioner: COOLING is on");
+    }
+}
---
+++ Pr2_2semestr/Pr2_2semestr/SecuritySystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class SecuritySystem
+{
+    public void Subscribe(TemperatureSensor sensor)
+    {
+        sensor.TemperatureChanged += CheckTemperature;
+    }
+
+    private void CheckTemperature(double temperature)
+    {
+        if (temperature > 40)
+            Console.WriteLine("SecuritySystem: System overheating!");
+        else if (temperature < 5)
+            Console.WriteLine("SecuritySystem: Risk of systems freezing!");
+    }
+}
