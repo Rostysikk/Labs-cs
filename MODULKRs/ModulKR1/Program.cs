d9adc81be8953585c5a4dda811fbb5b9dd9d53d8
Rostysikk <st9246435@stud.duikt.edu.ua>
2026-03-25 17:06:36 +0200

    no message

---
+++ MODULKRs/ModulKR1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public delegate string TextOperation(string text);
+
+    static void Main()
+    {
+        string inputFile = "textPD23.txt";
+        string outputFile = "resultPD23.txt";
+
+        File.WriteAllText(outputFile, "");
+
+        ProcessFile(inputFile, outputFile, ToUpperCase);
+        ProcessFile(inputFile, outputFile, CountCharacters);
+        ProcessFile(inputFile, outputFile, CountWords);
+
+        Console.WriteLine("Виконано!");
+    }
+
+    static void ProcessFile(string inputFile, string outputFile, TextOperation operation)
+    {
+        string text = File.ReadAllText(inputFile);
+
+        string result = operation(text);
+
+        File.AppendAllText(outputFile, result + Environment.NewLine + "-----" + Environment.NewLine);
+    }
+
+    static string ToUpperCase(string text)
+    {
+        return text.ToUpper();
+    }
+
+    static string CountCharacters(string text)
+    {
+        return "Кількість символів: " + text.Length;
+    }
+
+    static string CountWords(string text)
+    {
+        string[] words = text.Split(new char[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        return "Кількість слів: " + words.Length;
+    }
+}
---
+++ MODULKRs/ModulKR2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        MessagePublisher publisher = new MessagePublisher();
+        FileLogger logger = new FileLogger("logPD21.txt");
+
+        // підписка на подію
+        publisher.MessageSent += logger.HandleMessage;
+
+        // вводимо 4 рази
+        for (int i = 0; i < 4; i++)
+        {
+            Console.Write("Введіть текст: ");
+            string message = Console.ReadLine();
+
+            publisher.Send(message);
+        }
+
+        Console.WriteLine("Готово!");
+    }
+}
+
+// клас publisher
+class MessagePublisher
+{
+    // event
+    public event Action<string> MessageSent;
+
+    public void Send(string message)
+    {
+        MessageSent?.Invoke(message);
+    }
+}
+
+// клас logger
+class FileLogger
+{
+    private string filePath;
+
+    public FileLogger(string path)
+    {
+        filePath = path;
+    }
+
+    // обробник події
+    public void HandleMessage(string message)
+    {
+        string log = $"[{DateTime.Now:HH:mm:ss}] {message}";
+        File.AppendAllText(filePath, log + Environment.NewLine);
+    }
+}
